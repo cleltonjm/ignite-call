01e69bf29b4f23aa0f9d73683e33605c42041f12
Clelton <cleltonjm_95@hotmail.com>
2025-01-06 19:00:11 -0300

    Estilização do calendario

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -2,10 +2,10 @@ interface GetWeekDayParams {
   short?: boolean
 }
 
-export function getWeekDays({ short = false }: GetWeekDayParams) {
+export function getWeekDays({ short = false }: GetWeekDayParams = {}) {
     const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
     return Array.from(Array(7).keys())
-      .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
+      .map((day) => formatter.format(new Date(Date.UTC(2025, 5, day))))
       .map((weekDay) => {
         if (short) {
           return weekDay.substring(0, 3).toUpperCase()
