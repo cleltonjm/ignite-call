469a1f067fa558163bda2eb74693580366bc0144
Clelton <cleltonjm_95@hotmail.com>
2025-01-09 21:12:45 -0300

    Calculando dias do mês

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,21 +8,60 @@ import {
   CalendarHeader,
   CalendarTitle,
 } from './styles'
+import { useMemo, useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const nextMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(nextMonthDate)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    }).map((_, i) => {
+      return currentDate.subtract(i + 1, 'day')
+    }).reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2024</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous Month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next Month">
             <CaretRight />
           </button>
         </CalendarActions>
