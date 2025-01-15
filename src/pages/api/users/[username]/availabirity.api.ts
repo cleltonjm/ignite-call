09e889e3c3159862bf1a4eb2c7b092203e1f51db
Clelton <cleltonjm_95@hotmail.com>
2025-01-14 21:29:30 -0300

    Carregando horarios disponíveis

--- src/pages/api/users/[username]/availabirity.api.ts
+++ src/pages/api/users/[username]/availabirity.api.ts
@@ -54,5 +54,22 @@ export default async function handle(
         return startHour + i 
     },)
 
-    return res.json({ possibleTimes })
+    const blockedTimes = await prisma.scheduling.findMany({
+        select: {
+            date: true,
+        },
+        where: {
+            user_id: user.id,
+            date: {
+                gte: referenceDate.set('hour', startHour).toDate(), // greater than or equal
+                lte: referenceDate.set('hour', endHour).toDate(), // less than or equal
+            },
+        },
+    })
+
+    const availableTimes = possibleTimes.filter((time) => {
+        return !blockedTimes.some(blockedTime => blockedTime.date.getHours() === time,)
+    })
+
+    return res.json({ possibleTimes, availableTimes })
 }
