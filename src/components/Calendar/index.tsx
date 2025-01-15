1a3fd066fb6e3cbde68b46d2c277ea43cf21d097
Clelton <cleltonjm_95@hotmail.com>
2025-01-14 21:41:18 -0300

    Exibindo horários diferentes

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -100,8 +100,6 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
     return calendarWeeks
   }, [currentDate])
 
-  console.log(calendarWeeks)
-
   return (
     <CalendarContainer>
       <CalendarHeader>
