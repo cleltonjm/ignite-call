bcc2b3e5514319540b61e678f3737cf8935936ab
Clelton <cleltonjm_95@hotmail.com>
2025-01-23 21:12:37 -0300

    Otimizações de SEO

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -2,6 +2,7 @@ import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '../../../lib/prisma'
 import { Container, UserHeader } from './styles'
+import { NextSeo } from 'next-seo'
 
 interface ScheduleProps {
   user: {
@@ -13,13 +14,17 @@ interface ScheduleProps {
 
 export default function Schedule({ user }: ScheduleProps) {
   return (
-    <Container>
-      <UserHeader>
-        <Avatar src={user.avatarUrl} />
-        <Heading>{user.name}</Heading>
-        <Text>{user.bio}</Text>
-      </UserHeader>
-    </Container>
+    <>
+      <NextSeo title={`Agendar com ${user.name} | Ignite Call`} />
+
+      <Container>
+        <UserHeader>
+          <Avatar src={user.avatarUrl} />
+          <Heading>{user.name}</Heading>
+          <Text>{user.bio}</Text>
+        </UserHeader>
+      </Container>
+    </>
   )
 }
 
